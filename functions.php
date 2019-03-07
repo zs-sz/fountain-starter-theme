14efabc6a85aedc338e04a9580d9f5b949caa43e
deerfeeder <rev8shot@gmail.com>
2019-03-07 18:22:08 +0100

    initial scaffolding

---
+++ functions.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Timber starter-theme
+ * https://github.com/timber/starter-theme
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since   Timber 0.1
+ */
+
+if ( ! class_exists( 'Timber' ) ) {
+	add_action( 'admin_notices', function() {
+		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
+	});
+
+	add_filter('template_include', function( $template ) {
+		return get_stylesheet_directory() . '/static/no-timber.html';
+	});
+
+	return;
+}
+
+/**
+ * Sets the directories (inside your theme) to find .twig files
+ */
+Timber::$dirname = array( 'templates', 'views' );
+
+/**
+ * By default, Timber does NOT autoescape values. Want to enable Twig's autoescape?
+ * No prob! Just set this value to true
+ */
+Timber::$autoescape = false;
+
+
+/**
+ * We're going to configure our theme inside of a subclass of Timber\Site
+ * You can move this to its own file and include here via php's include("MySite.php")
+ */
+class StarterSite extends Timber\Site {
+	/** Add timber support. */
+	public function __construct() {
+		add_action( 'after_setup_theme', array( $this, 'theme_supports' ) );
+		add_filter( 'timber_context', array( $this, 'add_to_context' ) );
+		add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
+		add_action( 'init', array( $this, 'register_post_types' ) );
+		add_action( 'init', array( $this, 'register_taxonomies' ) );
+		parent::__construct();
+	}
+	/** This is where you can register custom post types. */
+	public function register_post_types() {
+
+	}
+	/** This is where you can register custom taxonomies. */
+	public function register_taxonomies() {
+
+	}
+
+	/** This is where you add some context
+	 *
+	 * @param string $context context['this'] Being the Twig's {{ this }}.
+	 */
+	public function add_to_context( $context ) {
+	
+		return $context;
+	}
+
+	public function theme_supports() {
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
+		add_theme_support( 'title-tag' );
+		add_theme_support( 'post-thumbnails' );
+		add_theme_support(
+			'html5', array(
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+			)
+		);
+
+		add_theme_support(
+			'post-formats', array(
+				'aside',
+				'image',
+				'video',
+				'quote',
+				'link',
+				'gallery',
+				'audio',
+			)
+		);
+
+		add_theme_support( 'menus' );
+	}
+
+	// public function myfilter( $text ) {
+	// 	$text .= '';
+	// 	return $text;
+	// }
+
+	public function add_to_twig( $twig ) {
+		$twig->addExtension( new Twig_Extension_StringLoader() );
+		// $twig->addFilter( new Twig_SimpleFilter( 'myfilter', array( $this, 'myfilter' ) ) );
+		return $twig;
+	}
+
+}
+
+new StarterSite();
